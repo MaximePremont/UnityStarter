18a3792a7d1a64c912185fcd06e1f2762bca9bfd
Maxime PREMONT <maxime.premont@gmail.com>
2022-08-14 16:35:38 +0200

    ADD: Auto hub download

--- StartUnity.sh
+++ StartUnity.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 FILE=./UnityHub.AppImage
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist, downloading it."
+    wget https://public-cdn.cloud.unity3d.com/hub/prod/UnityHub.AppImage
+    chmod +x $FILE
+fi
 $FILE </dev/null &>./logs.txt &
 exit
